39665548de8dff325eec5d35c172efe1d0ca12c6
Julian Lobon <julitolos6@gmail.com>
2026-03-05 09:30:47 -0500

    chore: agregar logo SVG, configurar estructura de estilos y establecer configuración de Vite
    - Añadir logo en formato SVG a la carpeta pública.
    - Crear archivo principal de React (main.jsx) y configurar el renderizado de la aplicación.
    - Definir fuentes personalizadas en font.css.
    - Establecer estilos globales en globals.css, incluyendo variables y estilos para componentes.
    - Crear archivo de variables CSS para gestionar espaciados y z-index.
    - Configurar Vite para resolver alias de rutas en el proyecto.

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@components": resolve(__dirname, "src/components"),
+      "@assets": resolve(__dirname, "src/assets"),
+      "@data": resolve(__dirname, "src/data"),
+      "@styles": resolve(__dirname, "src/styles"),
+    },
+  },
+});
